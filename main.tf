8ea34a37eb77bdfba1ddf95612a7c01cdccdfaaf
GayashanDeshapriya <gayashandeshapriya1922@gmail.com>
2024-01-24 22:21:59 +0530

    Updated main.tf file

--- main.tf
+++ main.tf
@@ -36,6 +36,15 @@ resource "aws_s3_bucket_public_access_block" "app" {
   restrict_public_buckets = false
 }
 
+resource "aws_s3_bucket_acl" "app" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.app,
+    aws_s3_bucket_public_access_block.app,
+  ]
+
+  bucket = aws_s3_bucket.app.id
+  acl    = "public-read"
+}
 
 resource "aws_s3_bucket_website_configuration" "app" {
   bucket = aws_s3_bucket.app.bucket
@@ -50,7 +59,7 @@ resource "aws_s3_bucket_website_configuration" "app" {
 }
 
 resource "aws_s3_object" "app" {
-  acl          = "public-read"
+  
   key          = "index.html"
   bucket       = aws_s3_bucket.app.id
   content      = file("./assets/index.html")
